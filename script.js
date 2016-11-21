a00be766f707fcf021ed71f51394edf6a4b19bc2
Tho Vo <tho.vo@ncr.com>
2016-11-21 14:58:41 +0100

    add test files

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 var git         = require 'ungit/source/git';
 var gitParser   = require 'ungit/source/git-parser';
+
+var task_git_show_conflict_list;
+var repository_path = 'https://github.com/thovo/testungit';
+
+task_git_show_conflict_list = git(['ls-files', '-u', '| cut -f 2', '| sort', '| uniq'], repository_path).parser(gitParser.parseGitLog).always(function(err, result) {
+  if (err != null) {
+    return console.log(err);
+  } else {
+    return console.log(result);
+  }
+});
+
+task_git_show_conflict_list.start();
